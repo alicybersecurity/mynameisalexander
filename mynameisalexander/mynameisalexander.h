3e4a498298369b7575df09685889931abc3bd1a1
Muhammad Ali Maniar <ali.maniar@purevpn.com>
2021-08-16 14:41:53 +0500

    Initial project setup

---
+++ mynameisalexander/mynameisalexander.h
@@ -0,0 +1,18 @@
+//
+//  mynameisalexander.h
+//  mynameisalexander
+//
+//  Created by Muhammad Ali Maniar on 16/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mynameisalexander.
+FOUNDATION_EXPORT double mynameisalexanderVersionNumber;
+
+//! Project version string for mynameisalexander.
+FOUNDATION_EXPORT const unsigned char mynameisalexanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mynameisalexander/PublicHeader.h>
+
+
