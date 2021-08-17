56c9a97b0ebe0a825f9771e4e94c2f9092278b32
Muhammad Ali Maniar <ali.maniar@purevpn.com>
2021-08-17 10:52:15 +0500

    Conquering Code

---
+++ mynameisalexander/Alexandar.swift
@@ -0,0 +1,15 @@
+//
+//  Alexandar.swift
+//  mynameisalexander
+//
+//  Created by Muhammad Ali Maniar on 16/08/2021.
+//
+
+import Foundation
+
+public final class Alexandar {
+    
+    public func areYouThere() -> String {
+        return "I am here"
+    }
+}
